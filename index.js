7d41f05cca022ba9d6e0bebcfb0eb0c0444f860c
Seth-Ramos <seth14438@gmail.com>
2025-06-26 18:28:31 -0700

    made express pages

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000
+
+app.get('/', (req, res) =>{
+  res.send('Hello!! add /home, /about, or /contact to find out more about me (or maybe smth else for suprise)')
+})
+
+app.get('/home', function(req, res){
+    res.sendFile(path.join(__dirname,'/home.html'));
+});
+
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname,'/about.html'));
+});
+
+app.get('/contact', function(req, res){
+    res.sendFile(path.join(__dirname,'/contact.html'));
+});
+  
+app.use((req, res, next) => {
+    res.status(404).send("Sorry nothin here! Look somewhere else lol")
+  });  
+
+
+app.listen(port,function() {
+  console.log(`Example app listening on port ${port}`)
+});
